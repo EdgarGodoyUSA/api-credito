3f4d6518b13d3c50eac412fc00732527ede2a7af
Edgar Godoy <s3curityinformation@gmail.com>
2022-05-14 10:14:55 -0500

    Se modifica servicio para leer los datos de S3

--- ejemplo_boto3.py
+++ ejemplo_boto3.py
@@ -1,14 +1,6 @@
-from fastapi import FastAPI, Form
-import joblib
 import boto3
+import joblib
 import os
-
-app = FastAPI()
-
-
-
-##pipeline = joblib.load("modelos/pipeline1.joblib")
-##modelo = joblib.load("modelos/model01.joblib")
 
 s3 = boto3.resource('s3', 'us-east-1')
 
@@ -28,15 +20,3 @@ print(model.predict(transformer.transform([['female', 80, 20000, 'free']])))
 
 print(model.predict_proba(transformer.transform([['female', 80, 20000, 'free']])))
 
-@app.post("/predict")
-def predict(sexo: str=Form(...),
-            edad: int=Form(...),
-            monto: int=Form(...),
-            tipovivienda: str=Form(...)):
-
-    X = transformer.transform([[sexo, edad, monto, tipovivienda]])
-    
-    proba_bad_client = model.predict_proba(X)[0][1]
-    
-
-    return {"proba_bad_client": proba_bad_client}
